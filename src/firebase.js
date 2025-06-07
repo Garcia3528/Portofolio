1626931a130755a7d9dc500f45c27cecc03bfc93
Garcia3528 <garcialucas0726@gmail.com>
2025-06-07 13:16:01 -0300

    Corrige conflitos de dependências no package.json

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,14 @@ import { getFirestore } from "firebase/firestore";
 import { collection, addDoc, getDocs } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDjZT-Zm1WcXyZPlTQXp7YqacI-DyEL1XU",
-  authDomain: "portfolio-2eca8.firebaseapp.com",
-  projectId: "portfolio-2eca8",
-  storageBucket: "portfolio-2eca8.firebasestorage.app",
-  messagingSenderId: "278938317933",
-  appId: "1:278938317933:web:728e946bb9be138ac83b64",
-  measurementId: "G-H3QN4R7FTF"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
